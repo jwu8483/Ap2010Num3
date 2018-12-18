3de4ac6f993ed79473e4ad2abb20af17fd7004cd
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2018-12-18 11:39:00 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public boolean isLevelTrailSegment(int start, int end){
+        int min = this.markers[start];
+        int max = this.markers[start];
+        for(int i = start +1; i <= end; i++
+        {
+            min = this.markers[i]
+                    
+        })
+    }
+}
